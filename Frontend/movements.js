cf0154478e5f3736272db1f4075f347ece5fc36a
rafinovalh <rafi.noval@ui.ac.id>
2023-06-10 22:59:38 +0700

    adding export

--- Frontend/movements.js
+++ Frontend/movements.js
@@ -1,3 +1,9 @@
-$(document).ready(function () {
-    $('#data-table').DataTable();
-});
+
+$(document).ready(function() {
+    $('#data-table').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+} );
